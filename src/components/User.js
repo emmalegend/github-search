71005c5f12c083c1d86da2113830db7fc561d080
Emmanuel <emmanuel.effiong361@gmail.com>
2021-01-26 03:42:15 +0100

    Refactoring

--- src/components/User.js
+++ src/components/User.js
@@ -20,7 +20,7 @@ const Wrapper = styled.div`
   @media (min-width: 992px) {
     grid-template-columns: 1fr 1fr;
   }
-  /* align-items: start; */
+  align-items: start; 
 `;
 
 export default User;
